a0ebf1452838b8716d9760e68240dfe097da385c
Maxwell McKee <maxwelljmckee@gmail.com>
2023-04-24 11:40:52 -0600

    adds status and task CRUD with stubbed UI

--- tutorial/todo/models.py
+++ tutorial/todo/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Status(models.Model):
-    name: models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
 
 class Task(models.Model):
-    title: models.CharField(max_length=250)
-    description: models.TextField()
-    status: models.ForeignKey(Status, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    statusId = models.ForeignKey(Status, on_delete=models.CASCADE)
+    status = models.CharField(max_length=64)
